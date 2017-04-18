ca41a6fff0bc18931e139f06501c09b3bb60eedd
Rajmund Szymanski <rajszym@o2.pl>
2017-04-18 16:04:43 +0200

    minor update

--- IntrOS/port/STM8S/osport.c
+++ IntrOS/port/STM8S/osport.c
@@ -2,7 +2,7 @@
 
     @file    IntrOS: osport.c
     @author  Rajmund Szymanski
-    @date    21.03.2017
+    @date    18.04.2017
     @brief   IntrOS port file for STM8 uC.
 
  ******************************************************************************
@@ -87,7 +87,7 @@ INTERRUPT_HANDLER(TIM3_UPD_OVF_BRK_IRQHandler, 15)
 
 #if defined(__SDCC)
 
-char port_get_lock(void) __naked
+char _get_CC(void) __naked
 {
 	__asm
 
@@ -98,9 +98,9 @@ char port_get_lock(void) __naked
 	__endasm;
 }
 
-void port_put_lock(char state) __naked
+void _set_CC(char cc) __naked
 {
-	(void) state;
+	(void) cc;
 	
 	__asm
 
