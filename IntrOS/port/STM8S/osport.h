9169684f8cef0ece2d0c89c6323f99c64dfc4ad2
Rajmund Szymanski <rajszym@o2.pl>
2017-04-22 16:07:46 +0200

    inclusion of device header file moved to osport.h

--- IntrOS/port/STM8S/osport.h
+++ IntrOS/port/STM8S/osport.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: osport.h
     @author  Rajmund Szymanski
-    @date    21.04.2017
+    @date    22.04.2017
     @brief   IntrOS port definitions for STM8 uC.
 
  ******************************************************************************
@@ -29,6 +29,7 @@
 #ifndef __INTROSPORT_H
 #define __INTROSPORT_H
 
+#include <stm8s.h>
 #include <osconfig.h>
 
 #ifdef __cplusplus
