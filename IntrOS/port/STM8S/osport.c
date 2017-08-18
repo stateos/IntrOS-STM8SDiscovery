6900c9f8607d05ae51ef36f87b34e597c41191fe
rajszym <rajszym@o2.pl>
2017-08-18 08:30:34 +0200

    IntrOS-STM8SDiscovery-SDCC has been merged into IntrOS-STM8SDiscovery

--- IntrOS/port/STM8S/osport.c
+++ IntrOS/port/STM8S/osport.c
@@ -2,7 +2,7 @@
 
     @file    IntrOS: osport.c
     @author  Rajmund Szymanski
-    @date    08.08.2017
+    @date    18.08.2017
     @brief   IntrOS port file for STM8 uC.
 
  ******************************************************************************
@@ -32,6 +32,11 @@
 
 void port_sys_init( void )
 {
+	CLK->CKDIVR = 0;
+	CLK->ECKR  |= CLK_ECKR_HSEEN; while ((CLK->ECKR & CLK_ECKR_HSERDY) == 0);
+	CLK->SWCR  |= CLK_SWCR_SWEN;
+	CLK->SWR    = 0xB4; /* HSE */ while ((CLK->SWCR & CLK_SWCR_SWBSY)  == 1);
+
 #if OS_TICKLESS == 0
 
 /******************************************************************************
@@ -39,18 +44,13 @@ void port_sys_init( void )
  It must generate interrupts with frequency OS_FREQUENCY
 *******************************************************************************/
 
-	CLK->CKDIVR = 0;
-	CLK->ECKR  |= CLK_ECKR_HSEEN; while ((CLK->ECKR & CLK_ECKR_HSERDY) == 0);
-	CLK->SWCR  |= CLK_SWCR_SWEN;
-	CLK->SWR    = 0xB4; /* HSE */ while ((CLK->SWCR & CLK_SWCR_SWBSY)  == 1);
-
 	#define  CNT_(X)   ((X)>>0?(X)>>1?(X)>>2?(X)>>3?(X)>>4?(X)>>5?(X)>>6?(X)>>7?(X)>>8?(X)>>9?1/0:9:8:7:6:5:4:3:2:1:0)
 	#define  PSC_ CNT_ ((CPU_FREQUENCY/OS_FREQUENCY-1)>>16)
 	#define  ARR_     (((CPU_FREQUENCY/OS_FREQUENCY)>>PSC_)-1)
 
 	TIM3->PSCR  = PSC_;
 	TIM3->ARRH  = ARR_ >> 8;
-	TIM3->ARRL  = ARR_;
+	TIM3->ARRL  = (uint8_t) ARR_;
 	TIM3->IER  |= TIM3_IER_UIE;
 	TIM3->CR1  |= TIM3_CR1_CEN;
 
